70767587f834c3ebebf738f6c4cb3b676be9c9f1
BraulioCadena <167551461+BraulioCadena@users.noreply.github.com>
2024-10-27 20:57:27 -0600

    9

--- scripts.js
+++ scripts.js
@@ -1,9 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const headerText = document.querySelector("header h1");
-    headerText.style.opacity = 0;
-
-    setTimeout(() => {
-        headerText.style.opacity = 1;
-        headerText.classList.add("float");
-    }, 1000);
+// Efecto de animación en el título
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector("header h1");
+    title.classList.add("title-animation");
 });
